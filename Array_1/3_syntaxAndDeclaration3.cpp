48d3585a6ff0dcf4e59bd4d151dc6f08c5739705
Swastika Ghosal <ghosalswastika2040@gmail.com>
2023-12-31 19:02:26 +0530

    Add files via upload

---
+++ Array_1/3_syntaxAndDeclaration3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[7]={1,5,9,4,6,3,1};
+    cout<<arr[0]<<" ";
+    cout<<arr[1]<<" ";
+    cout<<arr[2]<<" ";
+    cout<<arr[3]<<" ";
+    cout<<arr[4]<<" ";
+    cout<<arr[5]<<" ";
+    cout<<arr[6]<<" ";
+
+}
